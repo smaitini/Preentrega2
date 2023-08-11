bdd6da295610b44f5e88e42c9b69db4f870fbcd0
smaitini <smaitini@gmail.com>
2023-08-10 23:47:33 -0300

    Retoques varios

--- js/carrito.js
+++ js/carrito.js
@@ -1,18 +1,66 @@
 const carritoDeCompras = JSON.parse(localStorage.getItem("Productos"));
 
- const contenedorProductos= document.querySelector(".carrito-productos");
-//  console.log(carritoDeCompras);
- let precioTotal =0;
- carritoDeCompras.forEach(producto => {
-    precioTotal= parseFloat(producto.precio * producto.cantidad)
-     const div = document.createElement("div");
-     div.classList.add("carrito-producto");
-     div.innerHTML=`
-     <img class="imagen-producto" src=${producto.foto} alt="">
-     <div class="titulo-producto"><p>Producto</p><h3>${producto.producto}</h3></div>
-     <div class="titulo-producto"><p>Cantidad</p><h3>${producto.cantidad}</h3></div>
-     <div class="titulo-producto"><p>Precio Unitario</p><h3>${producto.precio}</h3></div>
-     <div class="titulo-producto"><p>Precio Total</p><h3>${precioTotal}</h3></div>`
-     contenedorProductos.append(div);
-});
-// [{"tipoProducto":"Futbol","idtipoPruducto":"1","idProducto":"1","producto":"Pelota nro 5 Profesional","precio":"1000","foto":"./img/pelota.jpg","cantidad":1}] 
+const contenedorProductos = document.querySelector(".carrito-productos");
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+let totalizador = document.querySelector("#total");
+
+let precioTotal = 0;
+let totalDeTotales = 0;
+
+function cargarProductosCarrito() {
+  totalDeTotales = 0;
+  contenedorProductos.innerHTML = "";
+  if (carritoDeCompras) {
+    carritoDeCompras.forEach((producto) => {
+      precioTotal = parseFloat(producto.precio * producto.cantidad);
+      totalDeTotales += precioTotal;
+      const div = document.createElement("div");
+      div.classList.add("carrito-producto");
+      div.innerHTML = `
+         <img class="carrito-producto-imagen" src=${producto.foto} alt="">
+         <div class="titulo-producto"><p>Producto</p><h3>${producto.producto}</h3></div>
+         <div class="titulo-producto"><p>Cantidad</p><h3>${producto.cantidad}</h3></div>
+         <div class="titulo-producto"><p>Precio Unitario</p><h3>${producto.precio}</h3></div>
+         <div class="titulo-producto"><p>Precio Total</p><h3>${precioTotal}</h3></div>
+         <div class="carrito-producto-eliminar" id=${producto.idProducto}>Eliminar</div>`;
+      contenedorProductos.append(div);
+      //totalizar(totalDeTotales); Lo pongo abajo sino cuando dejo el carrito vacio no me saca el utimo precio porque no entra por acá!!!!!!
+    });
+  } 
+  totalizar(totalDeTotales);
+  actualizarBotonesEliminar(); //Se tienen que cargar cada ver que haya algun movimiento en el carro de compras
+}
+
+cargarProductosCarrito(); //Se tiene que cargar cada vez que entra
+// [{"tipoProducto":"Futbol","idtipoPruducto":"1","idProducto":"1","producto":"Pelota nro 5 Profesional","precio":"1000","foto":"./img/pelota.jpg","cantidad":1}]
+
+function actualizarBotonesEliminar() {
+  botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+  botonesEliminar.forEach((boton) => {
+    boton.addEventListener("click", eliminarItemDelCarro);
+  });
+}
+
+function eliminarItemDelCarro(e) {
+  let idBotonito = e.currentTarget.id; //trae el valor del ID de "<div class="carrito-producto-eliminar" id=${producto.idProducto}>Eliminar</div>"
+  let i = carritoDeCompras.findIndex((item) => item.idProducto === idBotonito);
+  carritoDeCompras.splice(i, 1);
+  localStorage.setItem("Productos", JSON.stringify(carritoDeCompras)); //para guardar en LS el nuevo valor
+  cargarProductosCarrito();
+  Toastify({
+    text: "Producto Eliminado",
+    style: {
+      background: "linear-gradient(to right, #c1121f, #f48c06)",
+    },
+    offset: {
+      x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+      y: 10 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+    },
+  }).showToast();
+}
+
+function totalizar(valor) {
+  totalizador.innerHtml = "";
+  totalizador.innerHTML = `Total ${valor}`;
+console.log(totalizador)
+}
--- js/main.js
+++ js/main.js
@@ -1,131 +1,24 @@
-const productos = [
-    {
-        "tipoProducto":"Futbol",
-        "idtipoPruducto": "1",
-        "idProducto":"1",
-        "producto": "Pelota nro 5 Profesional",
-        "precio":"1000",
-        "foto": "./img/pelota.jpg"
-    },
-    {
-        "tipoProducto":"Futbol",
-        "idtipoPruducto": "1",
-        "idProducto":"2",
-        "producto":"Pelota",
-        "precio":"1000",
-        "foto": "./img/pelota.jpg"
-    },
-    {
-        "tipoProducto":"Futbol",
-        "idtipoPruducto": "1",
-        "idProducto":"3",
-        "producto": "Pelota Etrusco",
-        "precio":"1000",
-        "foto": "./img/etrusco.jpg"
-    },
-    {
-        "tipoProducto":"Futbol",
-        "idtipoPruducto": "1",
-        "idProducto":"4",
-        "producto": "Pelota Tango",
-        "precio":"1000",
-        "foto": "./img/Tango.jpg"
-    },
-    {
-        "tipoProducto":"Artes Marciales",
-        "idtipoPruducto": "2",
-        "idProducto":"5",
-        "producto": "Dobok TAEKWONDO",
-                "precio":"1000",
-        "foto": "./img/dobokTKD.jpg"
-    },
-    {
-        "tipoProducto":"Artes Marciales",
-        "idtipoPruducto": "2",
-        "idProducto":"6",
-        "producto": "Cinturon Amarillo",
-                "precio":"1000",
-        "foto": "./img/cinturonAmarillo.jpg"
-    },
-    {
-        "tipoProducto":"Artes Marciales",
-        "idtipoPruducto": "2",
-        "idProducto":"7",
-        "producto": "Cinturon Verde",
-        "precio":"1000",
-        "foto": "./img/cinturonVerde.jpg"
-    },
-    {
-        "tipoProducto":"Artes Marciales",
-        "idtipoPruducto": "2",
-        "idProducto":"8",
-        "producto": "Cinturon Azul",
-        "precio":"1000",
-        "foto": "./img/cinturonAzul.jpg"
-    },
-    {
-        "tipoProducto":"Artes Marciales",
-        "idtipoPruducto": "2",
-        "idProducto":"8",
-        "producto": "Cinturon Rojo",
-        "precio":"1000",
-        "foto": "./img/cinturonRojo.jpg"
-    },
-    {
-        "tipoProducto":"Artes Marciales",
-        "idtipoPruducto": "2",
-        "idProducto":"10",
-        "producto": "Cinturon Negro",
-        "precio":"1000",
-        "foto": "./img/cinturonNegro.jpg"
-    },
-    {
-        "tipoProducto":"Artes Marciales",
-        "idtipoPruducto": "2",
-        "idProducto":"11",
-        "producto": "Puntas cinturon",
-        "precio":"1000",
-        "foto": "./img/puntas.jpg"
-    },
-    {
-        "tipoProducto":"Running",
-        "idtipoPruducto": "3",
-        "idProducto":"12",
-        "producto": "Zapatillas Adidas Running Hombre",
-        "precio":"1000",
-        "foto": "./img/running_hombre1.jpg"
-    },
-    {
-        "tipoProducto":"Running",
-        "idtipoPruducto": "3",
-        "idProducto":"13",
-        "producto": "Zapatillas Adidas Running Mujer",
-        "precio":"1000",
-        "foto": "./img/running_mujer1.jpg"
-    },
-    {
-        "tipoProducto":"Hockey",
-        "idtipoPruducto": "4",
-        "idProducto":"14",
-        "producto": "Palo Hokey Master",
-        "precio":"1000",
-        "foto": "./img/paloHockey1.jpg"
-    },
-    {
-        "tipoProducto":"Hockey",
-        "idtipoPruducto": "4",
-        "idProducto":"15",
-        "producto": "Zapatillas Adidas Running Mujer",
-        "precio":"1000",
-        "foto": "./img/running_mujer1.jpg"
-    }
-]
-
+let productos;
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botones = document.querySelectorAll(".botonito");
 let botonesAgregas = document.querySelectorAll(".producto-agregar");
 
-productosCargados(productos);
+
+    fetch("../json/productos.json")
+    .then(respuesta => respuesta.json())
+    .then(respuesta => {
+        productos = respuesta;
+        productosCargados(productos);
+    }
+    )
+    .catch((e) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Los productos no han podido ser cargados',
+          })
+    })
+
 
 botones.forEach(boton => {
     boton.addEventListener("click",(e) => {
@@ -133,9 +26,6 @@ botones.forEach(boton => {
             productosCargados(productos);
         } else {
             const botondeProductos = productos.filter(producto => producto.tipoProducto === e.currentTarget.id);
-            // console.log(e.currentTarget.id);
-            // console.log("-------");
-            // console.log(botondeProductos);
             productosCargados(botondeProductos);
         }
 }
@@ -156,14 +46,14 @@ function productosCargados(x){
             <img class="producto-imagen" src=${producto.foto} alt="producto.producto">
             <div class="producto-detalles">
                 <h3 class="producto-titulo">${producto.producto}</h3>
-                <p class="producto-precio">${producto.precio}</p>
+                <p class="producto-precio">$ ${producto.precio}</p>
                 <button class="producto-agregar" id=${producto.idProducto}>Agregar</button>
-            </div>`;
-        
+            </div>`
         contenedorProductos.append(div);
-        })
+        console.log(div)
+    })
+
         actualizarBotonesAgregar();
-        // console.log(botonesAgregas);
 }
 /*--------------------------------------------*/
 function actualizarBotonesAgregar() {
@@ -171,7 +61,6 @@ function actualizarBotonesAgregar() {
     botonesAgregas.forEach(boton => {
         boton.addEventListener("click",agregaCarrito);
     })
-    // console.group(botonesAgregas);
 }
 
 let productosEnCarrito;
@@ -186,15 +75,23 @@ function agregaCarrito(e) {
     const id = e.currentTarget.id;
     const agregarProducto = productos.find(producto => producto.idProducto === id);
     if (productosEnCarrito.some(producto => producto.idProducto === id)){
-        //productosEnCarrito.push(productos.find(producto => producto.idProducto === id));
         const i = productosEnCarrito.findIndex(producto => producto.idProducto === id);
         productosEnCarrito[i].cantidad++;
     } else {    
         agregarProducto.cantidad=1;
         productosEnCarrito.push(agregarProducto);// no me sirve el "productos.find(producto => producto.idProducto === id)" porque sino lo le puedo agregar la cantidad al producto que voy a agregar. O lo hago desde aca, o le tengo que poner cantidad en el JSON. Pero esa cantidad seria el stock y aca la idea no es bajar el stock sino agregar al carrito
     }
+    Toastify({
+        text: "Producto agreado",
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+          },
+        offset: {
+          x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+          y: 10 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+      }).showToast();
     //Guardo en LS
     localStorage.setItem("Productos",JSON.stringify(productosEnCarrito));
-    // console.log(productosEnCarrito);
 }
 
